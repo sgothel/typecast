13b3636b72e18efe153f4fa591b0734f95f0fe14
Sven Gothel <sgothel@jausoft.com>
2023-02-11 20:10:45 +0100

    KernTable: Add direct access to KernSubtableFormat0, add toString()

--- src/main/java/net/java/dev/typecast/ot/table/KernTable.java
+++ src/main/java/net/java/dev/typecast/ot/table/KernTable.java
@@ -1,9 +1,9 @@
 /*****************************************************************************
  * Copyright (C) The Apache Software Foundation. All rights reserved.        *
- * ------------------------------------------------------------------------- * 
- * This software is published under the terms of the Apache Software License * 
- * version 1.1, a copy of which has been included with this distribution in  * 
- * the LICENSE file.                                                         * 
+ * ------------------------------------------------------------------------- *
+ * This software is published under the terms of the Apache Software License *
+ * version 1.1, a copy of which has been included with this distribution in  *
+ * the LICENSE file.                                                         *
  *****************************************************************************/
 
 package net.java.dev.typecast.ot.table;
@@ -16,27 +16,49 @@ import java.io.IOException;
  * @author <a href="mailto:david.schweinsberg@gmail.com">David Schweinsberg</a>
  */
 public class KernTable implements Table {
-    
-    private int version;
-    private int nTables;
-    private KernSubtable[] tables;
+
+    private final int version;
+    private final int nTables;
+    private final KernSubtable[] tables;
+    private final KernSubtableFormat0 table0;
 
     /** Creates new KernTable */
-    public KernTable(DataInput di) throws IOException {
+    public KernTable(final DataInput di) throws IOException {
         version = di.readUnsignedShort();
         nTables = di.readUnsignedShort();
         tables = new KernSubtable[nTables];
+        KernSubtableFormat0 _table0 = null;
         for (int i = 0; i < nTables; i++) {
             tables[i] = KernSubtable.read(di);
+            if( null == _table0 && 0 == tables[i].getSubtableFormat() ) {
+                _table0 = (KernSubtableFormat0)tables[i];
+            }
         }
+        table0 = _table0;
     }
 
     public int getSubtableCount() {
         return nTables;
     }
-    
-    public KernSubtable getSubtable(int i) {
+
+    public KernSubtable getSubtable(final int i) {
         return tables[i];
     }
 
+    public KernSubtableFormat0 getSubtable0() {
+        return table0;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder();
+        sb.append("'kern' Table\n--------------------------")
+          .append("\n  version:   ").append(version)
+          .append("\n  subtables: ").append(nTables);
+        for (int i = 0; i < nTables; i++) {
+            sb.append("\n  ");
+            sb.append(tables[i].toString());
+        }
+        return sb.toString();
+    }
 }
